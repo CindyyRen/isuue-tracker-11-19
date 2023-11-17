fbb2116376ffed920d1df93d3753bbb08dd7066d
Cindy Ren <bdcindy78@gmail.com>
2023-11-17 19:54:47 +1100

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { FilePlusIcon } from '@radix-ui/react-icons';
-import { Button, TextArea, TextField } from '@radix-ui/themes';
-import React from 'react';
+import { FilePlusIcon, InfoCircledIcon } from '@radix-ui/react-icons';
+import { Button, Callout, TextArea, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 import { useForm, Controller } from 'react-hook-form';
 import 'easymde/dist/easymde.min.css';
@@ -10,6 +10,7 @@ import { useRouter } from 'next/navigation';
 
 const NewIssuePage = () => {
   const router = useRouter();
+  const [error, setError] = useState('');
   const {
     register,
     handleSubmit,
@@ -17,32 +18,46 @@ const NewIssuePage = () => {
     formState: { errors },
   } = useForm();
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
-      })}
-    >
-      <TextField.Root>
-        {/* <TextField.Slot>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <InfoCircledIcon />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/issues0', data);
+            router.push('/issues');
+          } catch (error) {
+            setError('An unexpected error occurred.');
+          }
+        })}
+      >
+        <TextField.Root>
+          {/* <TextField.Slot>
           <MagnifyingGlassIcon height="16" width="16" />
         </TextField.Slot> */}
-        <TextField.Input placeholder="title" {...register('title')} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        rules={{ required: true }}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+          <TextField.Input placeholder="title" {...register('title')} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
 
-      <Button>
-        <FilePlusIcon width="16" height="16" /> submit issue
-      </Button>
-    </form>
+        <Button>
+          <FilePlusIcon width="16" height="16" /> submit issue
+        </Button>
+      </form>
+    </div>
   );
 };
 
