c9ee89f3599c2b2c85197f47c6b3279fb130034c
Cindy Ren <bdcindy78@gmail.com>
2023-11-18 21:59:00 +1100

    change navbar layout

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 import { DiBugsense } from 'react-icons/di';
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const links = [
   { label: 'Dashboard', href: '/' },
@@ -15,44 +15,54 @@ const Navbar = () => {
   const currentPath = usePathname();
   const { status, data: session } = useSession();
   return (
-    <nav className="flex space-x-1 md:space-x-6 border-b mb-5 h-14 items-center text-sm md:text-lg">
-      <div className="flex text-sm items-center  md:text-lg">
-        <Link href="/">
-          <DiBugsense className="inline w-20 h-20 text-blue-800 " />
-          {/* <span className="text-white bg-blue-700 p-1 rounded-full ">tra</span>
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <div className="flex text-sm items-center  md:text-lg pr-8">
+              <Link href="/">
+                {/* <DiBugsense className="inline w-12 h-12 text-blue-800 " /> */}
+                {/* <span className="text-white bg-blue-700 p-1 rounded-full ">tra</span>
         <span className="text-blue-800">Iss</span> */}
-        </Link>
-
-        <p className="flex items-bottom font-extrabold text-transparent text-xl bg-clip-text bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 bg-transparent h-full">
-          IssTracker
-        </p>
-      </div>
-      <ul className="flex  space-x-8 max-sm:hidden">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classNames({
-                'text-zinc-700 font-bold underline underline-offset-18':
-                  link.href === currentPath,
-                'text-zinc-500': link.href !== currentPath,
-                ' hover:text-zinc-800 transition-colors underline-offset-18':
-                  true,
-              })}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === 'authenticated' && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-        {status === 'unauthenticated' && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+              </Link>
+              {/* <p className="font-extrabold text-transparent text-xl  bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-transparent h-full"> */}
+                <p className="flex items-bottom font-extrabold text-transparent text-xl bg-clip-text bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 bg-transparent h-full">
+                ISSTra
+              </p>
+              {/* <span className="text-white font-extrabold bg-amber-500 p-1 rounded-full ">
+                ISS
+              </span>
+              <span className="text-amber-700 font-extrabold ">Tracker</span> */}
+            </div>
+            <ul className="flex  space-x-8 max-sm:hidden">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={classNames({
+                      'text-zinc-700 font-bold underline underline-offset-18':
+                        link.href === currentPath,
+                      'text-zinc-500': link.href !== currentPath,
+                      ' hover:text-zinc-800 transition-colors underline-offset-18':
+                        true,
+                    })}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'authenticated' && (
+              <Link href="/api/auth/signout">Log out</Link>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       textUnderlineOffset: {
-        18: '21px',
+        18: '19px',
       },
     },
   },
