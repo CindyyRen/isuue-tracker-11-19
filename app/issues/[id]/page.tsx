97206902a1e5d851cd4d7eb19a46ccaec25b8e3d
Cindy Ren <bdcindy78@gmail.com>
2023-11-19 17:44:31 +1100

    sort issues

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,16 +7,21 @@ import DeleteIssueButton from './DeleteIssueButton';
 import authOptions from '@/app/auth/authOptions';
 import { getServerSession } from 'next-auth';
 import AssigneeSelect from './AssigneeSelect';
+import { cache } from 'react';
 
 interface Props {
   params: { id: string };
 }
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
 
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchUser(parseInt(params.id));
+  // const issue = await prisma.issue.findUnique({
+  //   where: { id: parseInt(params.id) },
+  // });
 
   if (!issue) notFound();
 
