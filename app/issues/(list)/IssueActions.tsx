8db0be7694d10c6fee920e49c888753539ae8f84
Cindy Ren <bdcindy78@gmail.com>
2023-11-19 15:24:39 +1100

    filter component

--- app/issues/(list)/IssueActions.tsx
+++ app/issues/(list)/IssueActions.tsx
@@ -1,18 +1,20 @@
 import { FilePlusIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { Button, Flex } from '@radix-ui/themes';
 import Link from 'next/link';
 import React from 'react';
+import IssueStatusFilter from './IssueStatusFilter';
 
 const IssueActions = () => {
   return (
-    <div className="mb-5">
+    <Flex mb="5" justify="between">
+      <IssueStatusFilter />
       <Link href="/issues/new">
         <Button>
           <FilePlusIcon width="16" height="16" />
           New Issue
         </Button>
       </Link>
-    </div>
+    </Flex>
   );
 };
 
